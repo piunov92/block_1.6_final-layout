c08e427c07237c47e1e4820aed2548ec6ac1e2d1
Sergey Piunov <piunov92@gmail.com>
2024-01-29 12:09:41 +0300

    fix cards repair

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,15 +1,24 @@
-const techCards = document
+const cloneCards = document
   .querySelector('.main__description-brands')
   .cloneNode(true)
-document.querySelector('.main__description').appendChild(techCards)
+document.querySelector('.main__description').appendChild(cloneCards)
 
-// const mainDescription = document.querySelector('.main__description')
-// console.log(mainDescription)
-// const mainDescriptionTechCards = mainDescription.children[3]
-// mainDescriptionTechCards.className += ' .main__description-brands--tech'
-// console.log(mainDescriptionTechCards)
-// const mainDescriptionTechCardsSwiper =
-//   mainDescriptionTechCards.querySelector('.swiper')
-// mainDescriptionTechCardsSwiper.classList.remove('swiper--brands')
-// mainDescriptionTechCardsSwiper.className += ' swiper--tech'
-// console.log(mainDescriptionTechCards.children[1].children[0])
+const mainDescription = document.querySelector('.main__description')
+const mainDescriptionRepairCards = mainDescription.children
+mainDescriptionRepairCards[3].className +=
+  ' main__description-brands--repair-cards'
+
+const repairCards = document.querySelector(
+  '.main__description-brands--repair-cards'
+)
+repairCards.children.item(0).textContent = 'Ремонт различных видов техники'
+const repairCardsItem = repairCards.querySelector('.cards__item')
+repairCardsItem.className += ' cards__item--repair'
+repairCardsItem.children.item(0).remove()
+
+const spanElement = document.createElement('span')
+spanElement.textContent = 'Ремонт ноутбуков'
+repairCardsItem.prepend(spanElement)
+repairCardsItem.children.item(0).className = 'cards__brand cards__text'
+repairCardsItem.children.item(1).className += ' cards__btn--repair'
+console.log(repairCardsItem.children)
