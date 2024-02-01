671542fd85aab1235055cc99f7623710af5717ba
Sergey Piunov <piunov92@gmail.com>
2024-02-01 12:10:37 +0300

    fix modal

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -1,23 +1,41 @@
-const dialog = document.querySelector('.modal')
-const showDialog = document.querySelector('#show-dialog')
-const closeDialog = document.querySelector('#close-dialog')
+const modal = document.getElementsByClassName('modal')[0]
+const backdrop = document.querySelectorAll('.container').item(0).children[0]
+const showDialog = document.getElementById('show-dialog')
+const closeDialog = document.getElementById('close-dialog')
 
 showDialog.addEventListener('click', () => {
+  modal.style.display = 'block'
   document.body.style.overflow = 'hidden'
-  dialog.showModal()
+  backdrop.style.display = 'flex'
+  /*
+  //только для тега <dialog>
+  modal.showModal()
+  */
 })
 
 closeDialog.addEventListener('click', () => {
+  modal.style.display = 'none'
+  backdrop.style.display = 'none'
   document.body.style.overflow = 'initial'
-  dialog.close()
+  /*
+  //только для тега <dialog>
+  modal.close()
+  */
 })
 
-dialog.addEventListener('click', closeOnBackDropClick)
-
-function closeOnBackDropClick({ currentTarget, target }) {
-  const dialogElement = currentTarget
-  const isClickedOnBackDrop = target === dialogElement
-  if (isClickedOnBackDrop) {
-    dialogElement.close()
+const largeScreen = window.matchMedia('(min-width: 1440px)')
+largeScreen.addEventListener('change', function (event) {
+  backdrop.style.display = 'none'
+  if (event.matches) {
+    modal.style.display = 'block'
+  } else {
+    modal.style.display = 'none'
   }
-}
+})
+
+window.addEventListener('click', function (e) {
+  if (e.target === backdrop) {
+    modal.style.display = 'none'
+    backdrop.style.display = 'none'
+  }
+})
